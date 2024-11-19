7ebc4f311baaceee0b2af3fe4e50a36e00a44d72
ArkoOf22 <arkodeepkoley123@gmail.com>
2024-11-20 01:18:35 +0530

    Added service

--- src/main/java/com/example/BrowserStack/service/FileWatcherService.java
+++ src/main/java/com/example/BrowserStack/service/FileWatcherService.java
@@ -1,4 +1,51 @@
 package com.example.BrowserStack.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+@Service
 public class FileWatcherService {
+
+    private final static String FILE_NAME="log.txt";
+    private final static String READ_MODE="r";
+    private final static String DESTINATION="/topic/lag";
+    private long offset;
+    private final RandomAccessFile randomAccessFile;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    public FileWatcherService() throws IOException {
+        randomAccessFile = new RandomAccessFile(FILE_NAME, READ_MODE);
+        offset=initialOffset();
+    }
+    public void sendUpdates() throws IOException {
+        long fileLength=randomAccessFile.length();
+
+        randomAccessFile.seek(offset);
+
+        while (randomAccessFile.getFilePointer() < fileLength) {
+            String latestData=randomAccessFile.readLine();
+            String payLoad = "{\"content\":\""+latestData+"\"}";
+
+            messagingTemplate.convertAndSend(DESTINATION,payLoad);
+        }
+        offset=fileLength;
+    }
+
+    private long initialOffset() throws IOException {
+        int lines=0;
+
+        while (randomAccessFile.readLine()!=null) {
+            lines++;
+        }
+        if(lines>10) {
+            offset=lines-10;
+        }
+        return offset;
+    }
 }
