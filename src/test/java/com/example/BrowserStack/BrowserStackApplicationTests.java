2d952165b4043df0bc78416cbe498451689e103d
ArkoOf22 <arkodeepkoley123@gmail.com>
2024-11-19 23:51:14 +0530

    Doing the 2.5 hours browser Stack coding

---
+++ src/test/java/com/example/BrowserStack/BrowserStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BrowserStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowserStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
